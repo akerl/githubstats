4f4f6ac3cc90c39163897a8a7d3fe2b2c1b5e643
Les Aker <me@lesaker.org>
2013-09-22 18:24:12 -0400

    more helper functions

--- lib/contributions.rb
+++ lib/contributions.rb
@@ -5,18 +5,21 @@ require 'date'
 module Contributions
     class << self
         def new(*args)
-            self::User.new(*args)
+            self::Contributions.new(*args)
         end
     end
 
-    class User
-        attr_reader :user, :data
+    Datapoint = Struct.new(:date, :score)
+
+    class Contributions
+        attr_reader :user, :data, :last_updated
         attr_accessor :url
 
         def initialize(user, autoload=true)
             @user = user
             @url = "https://github.com/users/#@user/contributions_calendar_data"
-            @data = Contributions::Data.new
+            @data = []
+            @last_updated = nil
             update if autoload
         end
 
@@ -28,53 +31,39 @@ module Contributions
             "Contributions from #@user"
         end
 
-        def update
-            begin
-                tmp = JSON.parse Curl::Easy.perform(@url).body_str
-            rescue
-                raise 'Unable to load data from GitHub'
-            end
-            @data.update(tmp)
-        end
-    end
-
-    class Data
-        attr_reader :raw, :last_updated
-
-        def initialize
-            @raw = []
-            @last_updated = nil
-        end
-
-        def update(new_data)
-            begin
-                data = new_data.collect { |date, score| [Date::parse(date), score.to_i] }
-                timestamp = DateTime.now
-            rescue
-                raise "Failed to parse data"
-            end
-            @raw = data
-            @last_updated = timestamp
-        end
-
-        def inspect
-            to_s
-        end
-
-        def to_s
-            "Contribution data from #@last_updated"
+        def to_a
+            @data
         end
 
         def to_h
-            @raw.inject(Hash.new(0)) { |hash, (date, score)| hash[date] = score }
+            @data.inject(Hash.new(0)) { |hash, point| hash[point.date] = point.score ; hash }
         end
 
-        def to_a
-            @raw
+        def update
+            begin
+                raw_data = JSON.parse Curl::Easy.perform(@url).body_str
+            rescue
+                raise 'Unable to load data from GitHub'
+            end
+            begin
+                new_data = raw_data.collect { |date, score| Datapoint.new(Date::parse(date), score.to_i) }
+            rescue
+                raise "Failed to parse data"
+            end
+            @data = new_data
+            @last_updated = DateTime.now
         end
 
-        def today?
-            @raw.to_h[Date.today]
+        def today
+            to_h[Date.today]
+        end
+
+        def streak
+            @data.reverse.take_while{ |point| point.score > 0 }
+        end
+
+        def max
+            @data.max { |a, b| a.score <=> b.score }
         end
     end
 end
---
+++ test.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib/contributions'
+
+contrib = Contributions.new 'akerl'
+
+puts "Contribution data for #{contrib.user}:
+    Today's score: #{contrib.today}
+    Current streak: #{contrib.streak.length}
+    High score: #{contrib.max.score} on #{contrib.max.date}"
+
