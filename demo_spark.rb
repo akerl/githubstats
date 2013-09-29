4fd584be6484b00aa1fa6a212ddb81ca4bdaf7db
Les Aker <me@lesaker.org>
2013-09-29 16:14:38 -0400

    actual binary

--- demo_spark.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-$:.unshift('.')
-require 'lib/contributions'
-
-contrib = Contributions.new ARGV.first
-recent_scores = contrib.data.pop(50).collect { |point| point.score.to_s }
-system('spark', *recent_scores)
-
